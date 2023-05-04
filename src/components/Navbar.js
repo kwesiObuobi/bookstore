cc54803a91d0587781a41e03579c4efbe8a14dff
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-05-04 15:04:16 +0000

    Add Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => (
+  <div className="navbar-container">
+    <nav>
+      <h1 className="logo">Bookstore</h1>
+    </nav>
+  </div>
+);
+
+export default Navbar;
