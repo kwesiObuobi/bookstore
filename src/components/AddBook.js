b46789611cfb12f0e2e153ec03f73db4341cd9f0
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-05-05 10:58:19 +0000

    Add styling for AddBook component

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -5,7 +5,7 @@ const AddBook = () => (
   <div className="add-book-container">
     <h1 className="add-book-header">Add new book</h1>
     <form action="#">
-      <input type="text" placeholder="Book title" required />
+      <input type="text" placeholder="Book title" className="add-book-input" required />
       <select name="new-author" id="new-author">
         <option value="author-1">Author 1</option>
         <option value="author-2">Author 2</option>
