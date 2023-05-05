04f8cdc5d38dff3a4f7995f660e3107a4ad31d5a
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-05-05 10:36:48 +0000

    Add styling for BookCard component

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import '../styles/BookCard.css';
 
 const BookCard = () => {
   const [completed, setCompleted] = useState('0');
@@ -14,21 +15,21 @@ const BookCard = () => {
         <div className="book-title">The Hunger Games</div>
         <div className="book-author">Suzzane Collins</div>
         <div className="book-buttons">
-          <button type="button">Comments</button>
-          <button type="button">Remove</button>
-          <button type="button">Edit</button>
+          <button type="button" className="book-btn">Comments</button>
+          <button type="button" className="book-btn">Remove</button>
+          <button type="button" className="book-btn">Edit</button>
         </div>
+      </div>
 
-        <div className="progress-box">
-          <div className="percentage">{completed}</div>
-          <div className="completed-text">Completed</div>
-        </div>
+      <div className="progress-box">
+        <div className="percentage">{completed}</div>
+        <div className="completed-text">Completed</div>
+      </div>
 
-        <div className="chapter-box">
-          <p>current chapter</p>
-          <div className="chapter">Chapter 17: &quot;A Lesson Learned&quot;</div>
-          <button type="button">UPDATE PROGRESS</button>
-        </div>
+      <div className="chapter-box">
+        <p>current chapter</p>
+        <div className="chapter">Chapter 17: &quot;A Lesson Learned&quot;</div>
+        <button type="button" className="update-progress-btn">UPDATE PROGRESS</button>
       </div>
     </div>
   );
