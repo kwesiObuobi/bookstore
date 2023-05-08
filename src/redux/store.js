2b89ffb1288e0f5b606332b2739723e970b4e283
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-05-08 15:39:11 +0000

    Refactor project structure to use ducks pattern

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import bookReducer from '../features/book/bookSlice';
-import categoryReducer from '../features/category/categorySlice';
+import bookReducer from './features/book/bookSlice';
+import categoryReducer from './features/category/categorySlice';
 
 const store = configureStore({
   reducer: {
